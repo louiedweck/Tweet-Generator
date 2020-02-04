d525f072d6e9cac7dce8ec6cff57d93b24f8d1af
louiedweck <dweck.louis@gmail.com>
2020-02-03 16:13:45 -0800

    histogram and repeated words

---
+++ histogram.py
@@ -0,0 +1,36 @@
+def get_all_words(location):
+    '''opens text file and converts into array of strings '''
+    f = open(location, "r")
+    lines = f.readlines()
+    f.close()
+    # print(all_words)
+    words = []
+    for line in lines:
+        for word in line.split():
+            words.append(word)
+    return words
+
+
+def hist(text):
+    ''' Iterate over words and adds them to dictionary that totals amount of occurences (for each word)'''
+    histogram = dict()
+    for word in text:
+        if word not in histogram:
+            histogram[word] = 1
+        else:
+            histogram[word] += 1
+    return histogram
+
+
+def repeated_words(histogram):
+    list_multiples = []
+    for word, count in histogram.items():
+        if count > 1:
+            list_multiples.append(word)
+    return list_multiples
+
+
+if __name__ == '__main__':
+    words = get_all_words('book.txt')
+    histogram = hist(words)
+    print(repeated_words(histogram))
